b0016d0411d6b442e796e4493ba647bc480ec533
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-05-28 21:07:12 -0400

    breaking for tonight

--- js/app.js
+++ js/app.js
@@ -23,9 +23,48 @@ const init = (event) => {
     tie = false
     render()
 }
+const render = () => {
+    updateBoard()
+    updateMessage()
+}
+const updateBoard = () => {
+    board.forEach(square => {
+        [square] = squareEls
+        [square] = 'X'
+    });
+}
 
+const updateMessage = () => {
+    if (!winner && !tie) {
+        messageEl.textContent = `It is ${turn}'s turn`
+    }
+    else if (!winner && tie) {
+        messageEl.textContent = "tie"
+    }
+    else {
+        messageEl.textContent = "Player has won!"
+    }
+}
+const handleClick = (event) => {
+    const squareIndex = event.target.id
+    if (board[squareIndex]) {
+        return
+    }
+    if (winner) {
+        return
+    }
+    placePiece(squareIndex)
+}
+
+const placePiece = (index) => {
+    board[index] = turn
+}
+
+// const checkForWinner()
 
 /*----------------------------- Event Listeners -----------------------------*/
 window.addEventListener('load', init)
-
+squareEls.forEach(function (square) {
+    square.addEventListener('click', handleClick)
+})
 
