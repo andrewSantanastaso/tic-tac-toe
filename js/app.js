b27bf7c82a33eeb1042b505717fbfa0e9129d853
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-05-28 20:40:34 -0400

    init function and event listener created

--- js/app.js
+++ js/app.js
@@ -15,10 +15,17 @@ const squareEls = document.querySelectorAll('.sqr')
 const messageEl = document.querySelector('#message')
 
 /*-------------------------------- Functions --------------------------------*/
-
+const init = (event) => {
+    console.log('game initialized')
+    board = ['', '', '', '', '', '', '', '', '']
+    turn = 'X'
+    winner = false
+    tie = false
+    render()
+}
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+window.addEventListener('load', init)
 
 
